32ecdaa8b8b90f8cad4f3eed2e52e9632798856d
Hamid Amirkaveh <h.amirkaveh@gmail.com>
2021-02-23 15:28:58 +0330

    Adding a CommandLineRunner Bean to print all the spring bean to stdout on start up

--- src/main/java/com/pinket/springbootrest/SpringBootRestApplication.java
+++ src/main/java/com/pinket/springbootrest/SpringBootRestApplication.java
@@ -1,7 +1,12 @@
 package com.pinket.springbootrest;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringBootRestApplication {
@@ -10,4 +15,16 @@ public class SpringBootRestApplication {
         SpringApplication.run(SpringBootRestApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext context) {
+        return args -> {
+            System.out.println("Inspecting the beans provided by Spring Boot:");
+
+            String[] beanNames = context.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
+    }
 }
