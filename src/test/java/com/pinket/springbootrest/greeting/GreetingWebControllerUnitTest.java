bb57dee3e88582074cdf2e03c8df3f4c5c4a8583
Hamid Amirkaveh <h.amirkaveh@gmail.com>
2021-02-24 16:02:18 +0330

    minor change: make use of Matcher as containsString()

--- src/test/java/com/pinket/springbootrest/greeting/GreetingWebControllerUnitTest.java
+++ src/test/java/com/pinket/springbootrest/greeting/GreetingWebControllerUnitTest.java
@@ -8,7 +8,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -22,19 +23,17 @@ public class GreetingWebControllerUnitTest {
 
     @Test
     public void greeting() throws Exception {
-        String content = mvc.perform(MockMvcRequestBuilders.get(baseUrl)
+        mvc.perform(MockMvcRequestBuilders.get(baseUrl)
                 .accept(MediaType.TEXT_HTML))
                 .andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString();
-        assertThat(content).contains("<p>Hello, World!</p>");
+                .andExpect(content().string(containsString("<p>Hello, World!</p>")));
     }
 
     @Test
     public void greetingWithName() throws Exception {
-        String content = mvc.perform(MockMvcRequestBuilders.get(baseUrl + "?name=Test")
+        mvc.perform(MockMvcRequestBuilders.get(baseUrl + "?name=Test")
                 .accept(MediaType.TEXT_HTML))
                 .andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString();
-        assertThat(content).contains("<p>Hello, Test!</p>");
+                .andExpect(content().string(containsString("<p>Hello, Test!</p>")));
     }
 }
